5c195fbbc67e86219a19520786e78760d0bd99c9
Sengrar <95856738+Sengrar@users.noreply.github.com>
2026-01-30 22:41:58 +0530

    feat: modify Home, Navbar, Footer (html, css, js) and add hero-bg.jpg

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// =======Footer Logic========
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch(err => console.error("Footer load failed:", err));
+});

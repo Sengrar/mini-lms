bd6f3db8aab477e91d4f91b577a5f1740014b2bd
Sengrar <95856738+Sengrar@users.noreply.github.com>
2026-02-05 00:29:03 +0530

    feat: Update home, nav, footer, dashboard

--- js/main.js
+++ js/main.js
@@ -8,3 +8,47 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(err => console.error("Footer load failed:", err));
 });
+
+// =======Stats Count Animation========
+
+const counters = document.querySelectorAll('.count');
+let hasAnimated = false;
+
+const startCount = () => {
+  if (hasAnimated) return;
+  hasAnimated = true;
+
+  counters.forEach(counter => {
+    const target = +counter.getAttribute('data-target');
+    const speed = 200; // smaller = faster
+
+    const updateCount = () => {
+      const current = +counter.innerText;
+      const increment = Math.ceil(target / speed);
+
+      if (current < target) {
+        counter.innerText = current + increment;
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = formatNumber(target);
+      }
+    };
+
+    updateCount();
+  });
+};
+
+const formatNumber = num => {
+  if (num >= 1000000) return (num / 1000000) + "M+";
+  if (num >= 1000) return (num / 1000) + "K+";
+  return num + "+";
+};
+
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    startCount();
+  }
+}, { threshold: 0.4 });
+
+observer.observe(document.querySelector('.impact-stats'));
+
